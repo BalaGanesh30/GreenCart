daea0dd081eae62b11e591e9f2ad48792d4cd63f
BalaGanesh <135745561+BalaGanesh30@users.noreply.github.com>
2025-05-03 10:58:19 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -69,10 +69,11 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        secure: true, // Render uses HTTPS
+        sameSite: "none", // Required for cross-site cookies
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
+
       .status(200)
       .json({ success: true, user: { email: user.email, name: user.name } });
   } catch (error) {
