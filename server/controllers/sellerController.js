213cf33737147fa51cb7f771a166d2072239bfa5
BalaGanesh <135745561+BalaGanesh30@users.noreply.github.com>
2025-05-03 10:38:31 +0530

    Update sellerController.js

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -1,26 +1,27 @@
-//Seller Login : /api/seller/login
 import { generateToken } from "../middlewares/authToken.js";
 
+// Seller Login: POST /api/seller/login
 export const sellerLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     if (
       email === process.env.SELLER_EMAIL &&
       password === process.env.SELLER_PASSWORD
     ) {
       const token = generateToken(email);
-      res.cookie("sellerToken", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      });
 
-      return res.status(201).json({ success: true, message: "Logged In" });
+      res
+        .cookie("sellerToken", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        })
+        .status(200)
+        .json({ success: true, message: "Logged In" });
     } else {
-      return res
-        .status(400)
-        .json({ success: false, message: "Invalid Credentials" });
+      res.status(400).json({ success: false, message: "Invalid Credentials" });
     }
   } catch (error) {
     console.log(error.message);
@@ -28,7 +29,7 @@ export const sellerLogin = async (req, res) => {
   }
 };
 
-//Seller Auth : /api/seller/is-auth
+// Seller Auth Check: GET /api/seller/is-auth
 export const isSellerAuth = async (req, res) => {
   try {
     res.status(200).json({ success: true });
@@ -38,17 +39,17 @@ export const isSellerAuth = async (req, res) => {
   }
 };
 
-//Logout seller :  /api/seller/logout
-
+// Seller Logout: GET /api/seller/logout
 export const sellerLogout = async (req, res) => {
   try {
-    res.clearCookie("sellerToken", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    });
-
-    res.status(201).json({ success: true, message: "Logged Out" });
+    res
+      .clearCookie("sellerToken", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      })
+      .status(200)
+      .json({ success: true, message: "Logged Out" });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ success: false, message: error.message });
