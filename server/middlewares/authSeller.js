a6ceecf9f9a200d5adc14e0a9010a99c48869263
BalaGanesh <135745561+BalaGanesh30@users.noreply.github.com>
2025-05-03 10:56:24 +0530

    Update authSeller.js

--- server/middlewares/authSeller.js
+++ server/middlewares/authSeller.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 
 export const authSeller = async (req, res, next) => {
+  console.log("Incoming cookies:", req.cookies);
+
   const { sellerToken } = req.cookies;
   if (!sellerToken) {
     return res.status(401).json({ success: false, message: "Not Authorized" });
@@ -8,8 +10,7 @@ export const authSeller = async (req, res, next) => {
 
   try {
     const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET);
-    if (tokenDecode.id === process.env.SELLER_EMAIL) {
-      // req.sellerId = tokenDecode.email; // ✅ Attach to `req` directly
+    if (tokenDecode.email === process.env.SELLER_EMAIL) {
       next();
     } else {
       return res
@@ -17,7 +18,7 @@ export const authSeller = async (req, res, next) => {
         .json({ success: false, message: "Not Authorized" });
     }
   } catch (error) {
-    console.log(error.message);
+    console.log("Token Error:", error.message);
     res.status(500).json({ success: false, message: error.message });
   }
 };
